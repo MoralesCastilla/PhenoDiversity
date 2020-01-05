896714af3b74115542c973bd3b1bcb7b1c44a916
MoralesCastilla <moralescastilla@g.harvard.edu>
2020-01-05 22:00:15 +0100

    analyses code

--- analyses/02climate_bias_correct/04_save_GCMcorrected_bias_as_nc.R
+++ analyses/02climate_bias_correct/04_save_GCMcorrected_bias_as_nc.R
@@ -1,14 +1,9 @@
-#######################################################
-# Script to run mean replacement bias-correction globally in RC:
-#' Read in climate data from GCMs daily projections
-#' Get climatology (mean for each day from 1950-1990 for each site)
-#' bias correction for coastal cells in BEST data
-#'
-#'
+#'######################################################
+#' Script to save bias-corrected GCMs in 03_function_correcting_GCM_bias.R as .nc:
 #'
 #' Started 20th January 2017
-# Ignacio Morales-Castilla, Benjamin Cook & Elizabeth M. Wolkovich
-#######################################################
+#' Ignacio Morales-Castilla & Elizabeth M. Wolkovich
+#'######################################################
 
 ## remove objects
 rm(list=ls())
@@ -18,24 +13,32 @@ options(stringsAsFactors = FALSE)
 packs.to.extract<-list('raster','ncdf4','maptools','sp','foreach','doParallel','abind')
 lapply(packs.to.extract,require, character.only=T)
 
-target.folder="/n/wolkovich_lab/climatefuture/corrected_GCMs/"
 
-seqs<-list(seq(1,99,1),seq(100,198,1),seq(199,297,1),seq(298,365,1))
-list.years<-list(seq(1950,2005,1),seq(2006,2080,1),seq(2081,2100,1))
-#setwd("~/MEGA/Work_Harvard_postdoc/vitis/data/R/")
+## auxiliary functions and vectors
 substrRight <- function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
 }
 
-## loading
-#for(t in 1:1){
-#for(j in 2:4){
+# vectors to divide up the tasks across years and day of the year
+seqs<-list(seq(1,99,1),seq(100,198,1),seq(199,297,1),seq(298,365,1))
+list.years<-list(seq(1950,2005,1),seq(2006,2080,1),seq(2081,2100,1))
 
-
+#' Function to save as .nc files the results produced in the script
+#' '03_function_correcting_GCM_bias.R'
+#' @param GCMnum - number of GCM simulation to be corrected 
+#' @param t - each year period in vector list.years: value 1 = 1950-2005; 
+#'          value 2 = 2006-2080; value 3 = 2081-2100 
+#' @param target.folder - local folder where results from '03_function_correcting_GCM_bias.R'
+#'          are stored          
+#' @return list with two rasterStacks: GCM.bias.corr.min and GCM.bias.corr.max
+#'
+saveGCMcorr.as.nc <- function(j,GCMnum,target.folder){  
   
-  t=2
-  j=16
-  GCM.i<-paste("GCM.",j,'.',sep="")
+  
+  GCM.i<-paste("GCM.",GCMnum,'.',sep="")
+  
+  
+  ## declare lists to store results
   storing.list.mins<-list()
   storing.list.maxs<-list()
   
@@ -58,14 +61,29 @@ substrRight <- function(x, n){
   }
   
   GCM.bias.corr.min<-stack(unlist(storing.list.mins))
-  writeRaster(GCM.bias.corr.min,paste(target.folder,GCM.i,"Tmin",
-                                      min(list.years[[t]]),"-",max(list.years[[t]]),".nc",sep=""),format="CDF",overwrite=TRUE)
-  rm(GCM.bias.corr.min,storing.list.mins)
-  
   GCM.bias.corr.max<-stack(unlist(storing.list.maxs))
-  writeRaster(GCM.bias.corr.max,paste(target.folder,GCM.i,"Tmax",
-                                      min(list.years[[t]]),"-",max(list.years[[t]]),".nc",sep=""),format="CDF",overwrite=TRUE)
-  rm(GCM.bias.corr.max,storing.list.maxs)
-  
-  
-  
+ return(list(GCM.bias.corr.min,GCM.bias.corr.max))  
+}
+ 
+
+#### example of application ####
+
+target.folder <- "~/your.local.dir.here"
+
+a<-saveGCMcorr.as.nc(t,GCMnum)
+ 
+GCM.bias.corr.min<-a[[1]]
+GCM.bias.corr.max<-a[[2]]
+
+writeRaster(GCM.bias.corr.min,paste(target.folder,GCM.i,"Tmin",
+                                    min(list.years[[t]]),"-",
+                                    max(list.years[[t]]),".nc",sep=""),
+            format="CDF",overwrite=TRUE)
+rm(GCM.bias.corr.min,storing.list.mins)
+
+writeRaster(GCM.bias.corr.max,paste(target.folder,GCM.i,"Tmax",
+                                    min(list.years[[t]]),"-",
+                                    max(list.years[[t]]),".nc",sep=""),
+            format="CDF",overwrite=TRUE)
+rm(GCM.bias.corr.max,storing.list.maxs)
+
